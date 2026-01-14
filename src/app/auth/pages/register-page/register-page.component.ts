4320bed63e72c42bb8816226ad91c9d4db81de9d
RiosCoder <edson.rios2010@gmail.com>
2026-01-14 08:36:05 -0400

    Adicion de fb form builder para formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,34 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder)
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  /**
+   * name -> obligatorio
+   * email -> obligatorio y un email (Vaidators.email?)
+   * username -> obligatorio, minLength 6
+   * password -> obligatorio, minLength 6
+   * password2 -> obligatorio (confirmPassword seria un mejor nombre)
+   */
+
+  onSave() {
+    console.log(this.myForm.value());
+    this.myForm.markAllAsTouched();
+    this.myForm.reset();
+  }
+}
